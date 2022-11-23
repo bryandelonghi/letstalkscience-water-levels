f5ecf84f8c88bdf0058750cae5d2e508fac15030
bryandelonghi <113536458+bryandelonghi@users.noreply.github.com>
2022-11-23 13:22:03 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("reset")
+    basic.clearScreen()
+    water_level = 0
+    minimum_level = 0
+})
+let minimum_level = 0
+let water_level = 0
+basic.clearScreen()
+water_level = 0
+minimum_level = 0
+basic.forever(function () {
+    if (input.pinIsPressed(TouchPin.P0)) {
+        water_level = 1
+    }
+    if (input.pinIsPressed(TouchPin.P0) && input.pinIsPressed(TouchPin.P1)) {
+        water_level = 4
+    }
+    if (input.pinIsPressed(TouchPin.P0) && (input.pinIsPressed(TouchPin.P1) && input.pinIsPressed(TouchPin.P2))) {
+        water_level = 8
+    }
+    if (water_level > minimum_level) {
+        minimum_level = water_level
+        basic.showNumber(minimum_level)
+        basic.showString("cm")
+    }
+})
